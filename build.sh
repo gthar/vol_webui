a659b484aca115bd687dadced89af10ac3e2db33
gthar <r.illa.pujagut@gmail.com>
2019-05-01 23:36:16 +0200

    re-organized directories

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ function setVar {
     sed -E "s|^(${varName} = )None(.*)$|\1${val}\2|g"
 }
 
-< src/main.py \
+< src/server/main.py \
     setVar INSTALL_DIR \"${INSTALL_DIR}\" | \
     setVar PORT $WS_PORT > \
     build/bin/vol_webui.py
@@ -24,36 +24,36 @@ function setVar {
 chmod +x build/bin/vol_webui.py
 
 echo --- building alsa monitorer
-gcc src/alsa_events.c \
+gcc src/server/alsa_events.c \
     -std=gnu99 -Wall -pedantic -Wextra \
     -I/usr/include/alsa \
     -o build/bin/alsa_events \
     -lasound
 
 echo --- building static page
-python build_page.py \
-   --in_js src/main.js \
+python build_scripts/build_page.py \
+   --in_js src/client/main.js \
    --port $WS_PORT \
-   --in_scss src/style.scss \
-   --in_svg src/icon.svg \
-   --in_jinja src/index.jinja2 \
+   --in_scss src/client/style.scss \
+   --in_svg src/client/icon.svg \
+   --in_jinja src/client/index.jinja2 \
    --out_index build/share/vol_webui/www/index.html
 
 echo --- retrieving and preparing font
-python get_font.py \
+python build_scripts/get_font.py \
    --font_orig https://fonts.gstatic.com/s/opensans/v16/mem8YaGs126MiZpBA-UFVZ0e.ttf \
    --kept_chars "0123456789M()" \
    --out_font build/share/vol_webui/www/open_sans.ttf
 
 echo --- rendering nginx config file
-python render_nginx_conf.py \
+python build_scripts/render_nginx_conf.py \
     --in_file src/nginx.jinja2 \
     --port $UI_PORT \
     --install_dir $INSTALL_DIR \
     --out_file build/share/vol_webui/nginx.conf
 
 echo --- rendering systemd unit file
-python render_systemd_unit.py \
+python build_scripts/render_systemd_unit.py \
     --in_file src/systemd_unit.jinja2 \
     --out_file build/lib/systemd/system/vol_webui.service \
     --install_dir $INSTALL_DIR \
